d425579431916e156a39a6fd98705192ef5e77b5
Sudarshan <sudarshan@exampe.com>
2020-03-08 15:40:28 +0530

    first commit

---
+++ src/main/java/List/Listcalc/App.java
@@ -0,0 +1,13 @@
+package List.Listcalc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
